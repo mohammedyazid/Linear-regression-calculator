cdda27cdd33b1205d5acf16f9cff90df0953755a
Yazid <bentaib.mohammedyazid@univ-bechar.dz>
2022-11-07 17:45:57 +0100

    Second line regression added

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ def Regression():
     print(f"x̄={round(X_Bar,2)}\nȳ={round(Y_Bar,2)}")
     print(f"Var(x)={round(Var_X,2)}\nVar(y)={round(Var_Y,2)}")
     print(f"Cov(x,y)={round(Cov(table_x,table_y),2)}")
-    print(f"Y = {round(a,2)}X + {round(b,2)}")
+    print(f"Dy/x:y= {round(a,2)}X + {round(b,2)}")
+    print(f"Dx/y:y= {round(a_,2)}X + {round(b_,2)}")
 
 def Interpret():
     print(f"r={r}")
@@ -33,11 +34,13 @@ def Interpret():
 
 def Draw():
     plt.plot(table_x,table_y,'.')
-    plt.plot(table_x,[a*x+b for x in table_x])
+    plt.plot(table_x,[a*x+b for x in table_x],label=f"{round(a,2)}X+{round(b,2)}")
+    plt.plot(table_x,[a_*x+b_ for x in table_x],label=f"{round(a_,2)}X+{round(b_,2)}")
+    plt.legend()
     plt.show()
 
 def Calculate():
-    global X_Bar,Y_Bar,Var_X,Var_Y,Covar,a,b,S_ex,S_ey,r
+    global X_Bar,Y_Bar,Var_X,Var_Y,Covar,a,b,S_ex,S_ey,r,alpha,beta,a_,b_
     X_Bar=Average(table_x)
     Y_Bar=Average(table_y)
     Var_X=Var(table_x)
@@ -45,6 +48,10 @@ def Calculate():
     Covar=Cov(table_x,table_y)
     a=Covar/Var_X
     b=Y_Bar-a*X_Bar
+    alpha=Covar/Var_Y
+    beta=X_Bar-alpha*Y_Bar
+    a_= 1/alpha
+    b_= -beta/alpha
     S_ex=sqrt(Var(table_x))
     S_ey=sqrt(Var(table_y))
     r=Cov(table_x,table_y) / (S_ex*S_ey)
@@ -61,7 +68,6 @@ def Menu():
         elif Choice == 'interpret the results.': Interpret()
         elif Choice == 'Draw the regression line': Draw()
         else: break
-
 #   Main Program
 FillTable()
 Menu()
